b0015796b3658744698a3594d8bdab613d0149e5
Tazwer Shakil <tazwershakilshakil@gmail.com>
2025-10-27 23:16:22 +0600

    added category section with job card data from database

---
+++ src/Components/TabCategory.jsx
@@ -0,0 +1,43 @@
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+import JobCard from './JobCard';
+
+const TabCategory = ({ jobs }) => {
+    return (
+        <Tabs>
+            <div className='w-11/12 px-6 py-10 mx-auto'>
+                <h1 className='text-3xl font-semibold text-center text-gray-800 capitalize lg:text-3xl '>
+                    Browse Jobs By Categories
+                </h1>
+                <p className='max-w-3xl mx-auto my-8 pb-12 text-base/relaxed text-center text-gray-500 '>
+                    Three categories available for the time being. They are Web
+                    Development, Graphics Design and Digital Marketing. Browse them by
+                    clicking on the tabs below.
+                </p>
+                <TabList>
+                    <div className='flex  items-center justify-center'>
+                        <Tab>Web Development</Tab>
+                        <Tab>Graphics Designer</Tab>
+                        <Tab>Digital Marketing</Tab>
+                    </div>
+                </TabList>
+                <TabPanel>
+                    <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 py-6'>
+                        {
+                            jobs.map((job) => (<JobCard key={job._id} job={job}></JobCard>))
+                        }
+                    </div>
+                </TabPanel>
+                <TabPanel>
+                    <h2>Any content 2</h2>
+                </TabPanel>
+                <TabPanel>
+                    <h2>Any content 3</h2>
+                </TabPanel>
+
+            </div>
+        </Tabs >
+    );
+};
+
+export default TabCategory;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,14 @@
+import { useLoaderData } from "react-router-dom";
 import Slider from "../Components/Slider";
+import TabCategory from "../Components/TabCategory";
 
 const Home = () => {
+    const jobs = useLoaderData()
+    console.log(jobs);
     return (
-        <div className="py-10">
+        <div className="py-5 space-y-10">
             <Slider></Slider>
+            <TabCategory jobs={jobs}></TabCategory>
         </div>
     );
 };
